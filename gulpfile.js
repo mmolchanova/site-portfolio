a2f133a0ee62ac075bf4df102b871b86f857850c
Mariya Molchanova <molchanova.m2903@gmail.com>
2017-12-10 11:36:25 +0700

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+
+const paths = {
+    root: '.build',
+    templates: {
+        pages: 'src/templates/pages/*.pug',
+        src: 'src/templates/**/*.pug',
+        dest: 'build/assets/'
+    }
+}
+
+function templates() {
+    return gulp.src(paths.templates.pages)
+        .pipe(pug({ pretty: true }))
+        .pipe(gulp.dest(paths.root));
+}
+
+exports.templates = templates;
