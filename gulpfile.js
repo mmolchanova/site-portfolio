059339768e4d8b03debc1a725e9917d89ba6fa9a
Mariya Molchanova <molchanova.m2903@gmail.com>
2017-12-10 16:28:19 +0700

    first version of the assembly

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,27 @@
 const gulp = require('gulp');
 const pug = require('gulp-pug');
 
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+
+const del = require('del');
+
+const browserSync = require('browser-sync').create();
+
 const paths = {
-    root: '.build',
+    root: './build',
     templates: {
         pages: 'src/templates/pages/*.pug',
-        src: 'src/templates/**/*.pug',
-        dest: 'build/assets/'
+        src: 'src/templates/**/*.pug'
+    },
+    styles: {
+        src: 'src/styles/**/*.scss',
+        dest: 'build/assets/styles/'
+    },
+    images: {
+        src: 'src/images/**/*.*',
+        dest: 'build/assets/images/'
     }
 }
 
@@ -16,4 +31,42 @@ function templates() {
         .pipe(gulp.dest(paths.root));
 }
 
+function styles() {
+    return gulp.src('./src/styles/app.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(sourcemaps.write())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(paths.styles.dest));
+}
+
+function clean() {
+    return del(paths.root);
+}
+
+function watch() {
+    gulp.watch(paths.templates.src, templates);
+    gulp.watch(paths.styles.src, styles);
+}
+
+function server() {
+    browserSync.init({
+        server: paths.root
+    });
+    browserSync.watch(paths.root + '/**/*.*', browserSync.reload);
+}
+
+function images() {
+    return gulp.src(paths.images.src)
+        .pipe(gulp.dest(paths.images.dest));
+}
+
 exports.templates = templates;
+exports.styles = styles;
+exports.clean = clean;
+
+gulp.task('default', gulp.series(
+    clean,
+    gulp.parallel(styles, templates, images),
+    gulp.parallel(watch, server)
+));
